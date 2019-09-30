33b23d833ee0b378ed9b67fe1cd1ae070504f150
kkolman <kkolman@morgridge.org>
2019-09-30 14:36:26 -0500

    Add cmdline args, cut out intermediate csv

--- filter.py
+++ filter.py
@@ -2,76 +2,78 @@ import gffutils
 from Bio import SeqIO
 import csv
 import sqlite3
+import argparse
+import os
+from datetime import datetime
 
-# FIXME autogenerate db (filename)
-DB = 'gff_run1.db'
+parser = argparse.ArgumentParser(description='Filter GFF3 files.')
+parser.add_argument('gff3', help='the gff3 file to be filtered')
+parser.add_argument('fasta', help='the corresponding fasta file')
+parser.add_argument('-l', '--lengththreshold', type=float, 
+    help='Minimum threshold for length (some % of the length of the sequences in the fasta file) (Default: .5)')
+parser.add_argument('-s', '--scorethreshold', type=int, 
+    help='Minimum score threshold (Default: 0)')
+parser.add_argument('-o', '--output', help='Name of output file (Default: output_<datetime>.gff3)')
+args = parser.parse_args()
 
-db = gffutils.FeatureDB(DB, keep_order=True)
+LENGTH_THRESHOLD = args.lengththreshold if args.lengththreshold is not None else .5
+if LENGTH_THRESHOLD > 1 or LENGTH_THRESHOLD < 0:
+    raise ValueError('Length threshold must be a value between 0 and 1.')
+
+SCORE_THRESHOLD = args.scorethreshold if args.scorethreshold is not None else 0
+if SCORE_THRESHOLD < 0:
+    raise ValueError('Score threshold must be a value greater than or equal to zero.')
+
+gff3_file = args.gff3
+fasta_file = args.fasta
+output_file = args.output if args.output is not None else 'output_{}.gff3'.format(
+    datetime.now().strftime('%Y%m%d_%H%M%S'))
+db_name = 'gff3_{}.db'.format(datetime.now().strftime('%Y%m%d_%H%M%S'))
+
+DB = gffutils.create_db(gff3_file, db_name, keep_order=True)
+
+# get lengths of sequences from fasta file
 lengths = {}
-
-# FIXME change to the parsed flag
-filename = 'humpback_whale_megNov1_transcripts.final.fasta'
-
-
-for record in SeqIO.parse(filename, 'fasta'):
+for record in SeqIO.parse(fasta_file, 'fasta'):
     lengths[record.id] = len(record.seq)
-
-# get the length of the longest match for each transcript
-# if just comparing the matches to the ones in the fasta file, delete this
-#for a in db.features_of_type('match'):
-#    if not a.attributes.get('m_stop', False) or not a.attributes.get('m_start', False):
-#        continue
-#
-#    length = int(a.attributes['m_stop'][0]) - int(a.attributes['m_start'][0])
-#    gene = a.attributes['match_id'][0]
-#
-#    if not matches.get(gene, False):
-#        matches[gene] = length
-#    else:
-#        if matches[gene] < length:
-#            matches[gene] = length
 
 # filter matches
 to_delete = {}
-for a in db.features_of_type('match'):
+for a in DB.features_of_type('match'):
     if not a.attributes.get('m_stop', False):
         continue
-    if not a.attributes.get('m_start', False):
-        start = 0
 
     start = 0 if 'm_start' not in a.attributes else int(a.attributes['m_start'][0])
     length = int(a.attributes['m_stop'][0]) - start
-    gene = a.attributes['match_id'][0]
+    score = int(a[5])
+    match = a.attributes['match_id'][0]
     gid = a.attributes['ID'][0]
 
-    if length < .5 * lengths[gene]:
-        to_delete.update({x.attributes['ID'][0]: 1 for x in [a, *list(db.children(gid))]})
-        #to_delete.extend([a, *list(db.children(gid))])
-        #db.delete([a, *list(db.children(gid))])
+    if length < LENGTH_THRESHOLD * lengths[match]:
+        to_delete.update({x.attributes['ID'][0]: 1 for x in [a, *list(DB.children(gid))]})
+    elif score < SCORE_THRESHOLD:
+        to_delete.update({x.attributes['ID'][0]: 1 for x in [a, *list(DB.children(gid))]})
 
-#db.delete(to_delete)
+# include gff directive at top
+f = open(output_file, 'w')
+f.write('##gff-version 3\n')
+f.close()
 
-fn = 'data1.csv'
-with sqlite3.connect(DB) as connection:
-    writer = csv.writer(open(fn, 'w'))
+# write filtered data to gff3 file
+with sqlite3.connect(db_name) as connection:
+    writer = csv.writer(open(output_file, 'a'), delimiter='\t')
     c = connection.cursor()
     c.execute('SELECT * FROM features')
-    rows = c.fetchall()
-    for x in rows:
-        writer.writerow(x)
+    for row in c:
+        attrs = eval(row[9])
+        if row[0] in to_delete:
+            continue
 
-reader = csv.reader(open(fn, 'r'))
-writer = csv.writer(open('output1.csv', 'w'), delimiter='\t', lineterminator='\n')
+        new_str = []
+        for k, v in attrs.items():
+            if k != 'original_id' and k != 'original_name':
+                new_str.append("{}={};".format(k, v[0]))
+        new_str = "".join(new_str)
+        writer.writerow([row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], new_str])
 
-next(reader)
-for row in reader:
-    new_row = eval(row[9])
-    if new_row['ID'][0] in to_delete:
-        continue
-    new_str = []
-    for k, v in new_row.items():
-        if k != 'original_id' or k != 'original_name':
-            new_str.append("{}={};".format(k, v[0]))
-    new_str = "".join(new_str)
-    writer.writerow([row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], new_str])
-
+os.remove(db_name)
